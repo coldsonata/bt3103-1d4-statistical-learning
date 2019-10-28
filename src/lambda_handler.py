3d9b6e130beca09fcc958728ece3ac9b32e4b0d5
Blanc <coldsonata@gmail.com>
2019-10-29 01:20:05 +0800

    sam

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -1,12 +1,24 @@
 import json
+import time
+import requests
+import os
 import boto3
 from datetime import datetime
 from content_parser import *
 from question_checker import *
-import statsmodels.formula as smf
-import numpy as np
-import pandas as pd
 
+dynamodb = boto3.resource('dynamodb')
+
+def save_logs(logData):
+    timestamp = str(datetime.utcnow().timestamp())
+
+    table = dynamodb.Table('loggingTable')
+    
+    log = logData.copy() 
+    log['itemId'] = str(timestamp) 
+    log['createdAt'] = timestamp
+
+    table.put_item(Item=log)
 
 def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
@@ -15,21 +27,7 @@ def lambda_handler(event, context):
         path = event.get('path')
         return content_parser(path)
     if method == 'POST':
-        
-        timestamp = str(datetime.utcnow().timestamp())
-
-        table = dynamodb.Table('loggingTable')
-        
-        log = {}
-    
-        log['itemId'] = str(timestamp) #str(uuid.uuid1()) for more granular keys
-        log['createdAt'] = timestamp
-    
-
-        # write logData to dynamoDB
-        table.put_item(Item=log)
-        
-        
-        postReq = json.loads(event.get('body', {}))
+        postReq = json.loads(event.get('body', {})) 
+        save_logs(postReq)
         return question_checker(postReq)
         
